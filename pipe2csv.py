824688b0e88b74a56457b0a072f5388150ad6109
Emrys-Hong <hongpengfei.emrys@gmail.com>
2019-06-04 19:01:56 +0800

    explore for char span in pdtb3

--- pipe2csv.py
+++ pipe2csv.py
@@ -8,6 +8,7 @@ def read_file(filename, fields_dict, index):
     for relation in rawtext:
         relation = relation.strip()
         fields = relation.split('|')
+        fields = [str(filename).split('/')[-1]] + fields
         fields_dict[index] = fields
         index+=1
     return fields_dict, index
@@ -17,7 +18,7 @@ def generate_df(main_foldername='/home/pengfei/data/PDTB-3.0/data/gold/'):
     filepaths = get_filepaths(main_foldername)
     
     fields_dict = {}
-    cols = ['Relation_Type','Conn_SpanList','Conn_Src','Conn_Type','Conn_Pol','Conn_Det','Conn_Feat_SpanList','Conn1','SClass1A','SClass1B','Conn2','SClass2A','SClass2B','Sup1_SpanList','Arg1_SpanList','Arg1_Src','Arg1_Type','Arg1_Pol','Arg1_Det','Arg2_Feat_SpanList','Arg2_SpanList','Arg2_Src','Arg2_Type','Arg2_Pol','Arg2_Det','Arg2_Feat_SpanList','Sup2_SpanList','Adju_Reason','Adju_Disagr','PB_Role','PB_Verb','Offset','Provenance','Link']
+    cols = ['DocID','Relation_Type','Conn_SpanList','Conn_Src','Conn_Type','Conn_Pol','Conn_Det','Conn_Feat_SpanList','Conn1','SClass1A','SClass1B','Conn2','SClass2A','SClass2B','Sup1_SpanList','Arg1_SpanList','Arg1_Src','Arg1_Type','Arg1_Pol','Arg1_Det','Arg1_Feat_SpanList','Arg2_SpanList','Arg2_Src','Arg2_Type','Arg2_Pol','Arg2_Det','Arg2_Feat_SpanList','Sup2_SpanList','Adju_Reason','Adju_Disagr','PB_Role','PB_Verb','Offset','Provenance','Link']
     index=0
     for filepath in filepaths:
         fields_dict, index = read_file(filepath, fields_dict, index)
@@ -31,6 +32,14 @@ def get_filepaths(main_foldername):
         for filename in os.listdir(main_foldername/folder):
             filepaths.append(main_foldername/folder/filename)
     return filepaths
+
+
+
+#write_folder = '/home/pengfei/data/PDTB-3.0/all/raw/'
+#for filename in filepaths:
+#    rawtext = codecs.open(filename, encoding='utf-8', errors='ignore').read()
+#    with open(write_folder + str(filename).split('/')[-1], 'a') as f:
+#        f.write(rawtext)
 
 if __name__ == "__main__":
     main_foldername = '/home/pengfei/data/PDTB-3.0/data/gold/'
